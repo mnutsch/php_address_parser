114af69402688900ea9a5cb710dc3d702109e2f4
mnutsch <mattnutsch@gmail.com>
2013-05-21 15:06:43 -0600

    Create country_array.php

---
+++ country_array.php
@@ -0,0 +1,66 @@
+<?php
+
+global $country_array;
+
+$country_array = array();
+
+$country_array[0]="argentina";
+$country_array[1]="australia";
+$country_array[2]="austria";
+$country_array[3]="baden-wurttemberg";
+$country_array[4]="belgium";
+$country_array[5]="brazil";
+$country_array[6]="bulgaria";
+$country_array[7]="canada";
+$country_array[8]="chile";
+$country_array[9]="china";
+$country_array[10]="colombia";
+$country_array[11]="croatia";
+$country_array[12]="czech republic";
+$country_array[13]="denmark";
+$country_array[14]="egypt";
+$country_array[15]="france";
+$country_array[16]="georgia";
+$country_array[17]="germany";
+$country_array[18]="hungary";
+$country_array[19]="india";
+$country_array[20]="ireland";
+$country_array[21]="italy";
+$country_array[22]="japan";
+$country_array[23]="kansas";
+$country_array[24]="l'vivs'ka";
+$country_array[25]="malaysia";
+$country_array[26]="mexico";
+$country_array[27]="morocco";
+$country_array[28]="netherlands";
+$country_array[29]="new zealand";
+$country_array[30]="nigeria";
+$country_array[31]="norway";
+$country_array[32]="poland";
+$country_array[33]="portugal";
+$country_array[34]="romania";
+$country_array[35]="russia";
+$country_array[36]="saudi arabia";
+$country_array[37]="south africa";
+$country_array[38]="south korea";
+$country_array[39]="spain";
+$country_array[40]="sweden";
+$country_array[41]="switzerland";
+$country_array[42]="taiwan";
+$country_array[43]="thailand";
+$country_array[44]="ukraine";
+$country_array[45]="united arab emirates";
+$country_array[46]="united kingdom";
+$country_array[47]="united states";
+$country_array[48]="united states of america";
+$country_array[49]="usa";
+$country_array[50]="us";
+$country_array[51]="u.s.a.";
+$country_array[52]="u.s.";
+$country_array[53]="vietnam";
+$country_array[54]="zimbabwe";
+
+
+
+
+?>
